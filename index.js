8d32261a170f0129cfdf346d8bdd126f11205f91
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-10-21 17:11:13 +0600

    Single and multiple item get request & put request created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
 
         const gadgetsCollection = client.db("gadgetsDB").collection("gadgets");
 
@@ -35,13 +35,37 @@ async function run() {
             res.send(result);
         });
 
-        // specific get request
+        // get request
         app.get("/products/:brand", async (req, res) => {
             const brand = req.params.brand;
             const query = {brand: brand};
             const result = await gadgetsCollection.find(query);
             const data = await result.toArray();
             res.send(data);
+        });
+
+        // get single item
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await gadgetsCollection.findOne(query);
+            res.send(result);
+        });
+
+        // update single item
+        app.put("/update-product/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const option = {upsert: true};
+            const product = req.body;
+            const { productName, brand, productType, price, ratings, details, photo } = product;
+            const updatedProduct = {
+                $set: {
+                    productName, brand, productType, price, ratings, details, photo
+                }
+            };
+            const result = await gadgetsCollection.updateOne(filter, updatedProduct, option);
+            res.send(result);
         });
 
         // Send a ping to confirm a successful connection
