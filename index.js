d5bac3f99837d7af14fff7a7c30452882da4d33f
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-10-30 07:49:33 +0600

    Fixed some bugs

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const gadgetsCollection = client.db("gadgetsDB").collection("gadgets");
         const cartItemsCollection = client.db("cartItemsDB").collection("cart");
